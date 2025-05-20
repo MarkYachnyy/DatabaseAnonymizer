1558f45988862fecd0a0f244ca822dae1079aa2a
MarkYachnyy <markyachny@gmail.com>
2025-05-20 16:02:07 +0300

    Add pravki to course paper, add presentation

--- src/main/java/ru/vsu/cs/iachnyi_m_a/database_anonymizer/Main.java
+++ src/main/java/ru/vsu/cs/iachnyi_m_a/database_anonymizer/Main.java
@@ -18,30 +18,13 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, SQLException {
         HikariDataSource source = HikariDataSourceFactory.create("anonymizer", "postgres", "1234", 1000, 10);
         DatabaseGenerator generator = new DatabaseGenerator(new QueryTool(source));
         DatabaseSchema schema = new Gson().fromJson(readFileToString("./test_data/schema.json"), DatabaseSchema.class);
         ConstraintSet constraintSet = new Gson().fromJson(readFileToString("./test_data/constraint.json"), ConstraintSet.class);
         RuleSet ruleSet = new Gson().fromJson(readFileToString("./test_data/rules.json"), RuleSet.class);
-        new Thread(() -> {
-            for (int i = 1; i < 10; i++) {
-                GlobalTimeMeasurer.setInitTime();
-                try {
-                    generator.fillDatabase(schema, constraintSet, ruleSet, "users", i * 1000);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-                while (!GlobalTimeMeasurer.finished){
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-
-                    }
-                }
-            }
-        }).start();
-
+        generator.fillDatabase(schema, constraintSet, ruleSet, "users", 1000);
     }
 
     public static String readFileToString(String filePath) throws IOException {
